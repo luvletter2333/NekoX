NextAlone/Nagram

[fork] Revert "Remove customApi and Disable dev mode for users"

TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
@@ -293,7 +293,7 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
     }
 
     private int menu_other = 5;
-//    private int menu_custom_api = 6;
+    private int menu_custom_api = 6;
     private int menu_custom_dc = 7;
     private int menu_qr_login = 8;
 
@@ -409,6 +409,153 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
                             editText.setLayoutParams(layoutParams);
                         }
                     }
+                } else if (id == menu_custom_api) {
+
+                    AtomicInteger targetApi = new AtomicInteger(-1);
+
+                    BottomBuilder builder = new BottomBuilder(getParentActivity());
+
+                    EditText[] inputs = new EditText[2];
+
+                    builder.addTitle(LocaleController.getString("CustomApi", R.string.CustomApi),
+                            true,
+                            LocaleController.getString("UseCustomApiNotice", R.string.UseCustomApiNotice));
+
+                    builder.addRadioItem(LocaleController.getString("CustomApiNo", R.string.CustomApiNo), NekoXConfig.customApi == 0, (cell) -> {
+
+                        targetApi.set(0);
+
+                        builder.doRadioCheck(cell);
+
+                        for (EditText input : inputs) input.setVisibility(View.GONE);
+
+                        return Unit.INSTANCE;
+
+                    });
+
+                    builder.addRadioItem(LocaleController.getString("CustomApiOfficial", R.string.CustomApiOfficial), NekoXConfig.customApi == 1, (cell) -> {
+
+                        targetApi.set(1);
+
+                        builder.doRadioCheck(cell);
+
+                        for (EditText input : inputs) input.setVisibility(View.GONE);
+
+                        return Unit.INSTANCE;
+
+                    });
+
+                    builder.addRadioItem(LocaleController.getString("CustomApiTGX", R.string.CustomApiTGX), NekoXConfig.customApi == 2, (cell) -> {
+
+                        targetApi.set(2);
+
+                        builder.doRadioCheck(cell);
+
+                        for (EditText input : inputs) input.setVisibility(View.GONE);
+
+                        return Unit.INSTANCE;
+
+                    });
+
+                    builder.addRadioItem(LocaleController.getString("CustomApiInput", R.string.CustomApiInput), NekoXConfig.customApi > 2, (cell) -> {
+
+                        targetApi.set(3);
+
+                        builder.doRadioCheck(cell);
+
+                        for (EditText input : inputs) input.setVisibility(View.VISIBLE);
+
+                        return Unit.INSTANCE;
+
+                    });
+
+                    inputs[0] = builder.addEditText("App Id");
+                    inputs[0].setInputType(InputType.TYPE_CLASS_NUMBER);
+                    if (NekoXConfig.customAppId != 0) {
+                        inputs[0].setText(NekoXConfig.customAppId + "");
+                    }
+                    inputs[0].addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+                            if (StrUtil.isBlank(s.toString())) {
+                                NekoXConfig.customAppId = 0;
+                            } else if (!NumberUtil.isInteger(s.toString())) {
+                                inputs[0].setText("0");
+                            } else {
+                                NekoXConfig.customAppId = NumberUtil.parseInt(s.toString());
+                            }
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+                        }
+                    });
+
+                    inputs[1] = builder.addEditText("App Hash");
+                    inputs[1].setFilters(new InputFilter[]{new InputFilter.LengthFilter(BuildVars.OFFICAL_APP_HASH.length())});
+                    if (StrUtil.isNotBlank(NekoXConfig.customAppHash)) {
+                        inputs[1].setText(NekoXConfig.customAppHash);
+                    }
+                    inputs[1].addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+                            NekoXConfig.customAppHash = s.toString();
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+                        }
+                    });
+
+                    if (NekoXConfig.customApi <= 2) {
+
+                        for (EditText input : inputs) input.setVisibility(View.GONE);
+
+                    }
+
+                    builder.addCancelButton();
+
+                    builder.addButton(LocaleController.getString("Set", R.string.Set), (it) -> {
+
+                        int target = targetApi.get();
+
+                        if (target > 2) {
+
+                            if (NekoXConfig.customAppId == 0) {
+
+                                inputs[0].requestFocus();
+                                AndroidUtilities.showKeyboard(inputs[0]);
+
+                                return Unit.INSTANCE;
+
+                            } else if (StrUtil.isBlank(NekoXConfig.customAppHash)) {
+
+                                inputs[1].requestFocus();
+                                AndroidUtilities.showKeyboard(inputs[1]);
+
+                                return Unit.INSTANCE;
+
+                            }
+
+                        }
+
+                        NekoXConfig.customApi = target;
+                        NekoXConfig.saveCustomApi();
+
+                        return Unit.INSTANCE;
+
+                    });
+
+                    builder.show();
+
                 } else if (id == menu_custom_dc) {
 
                     AtomicInteger targetDc = new AtomicInteger(-1);
@@ -498,7 +645,7 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
 
         otherItem.addSubItem(4, R.drawable.list_bot, LocaleController.getString("BotLogin", R.string.BotLogin));
         otherItem.addSubItem(menu_qr_login, R.drawable.wallet_qr, LocaleController.getString("ImportLogin", R.string.ImportLogin));
-//        otherItem.addSubItem(menu_custom_api, R.drawable.baseline_vpn_key_24, LocaleController.getString("CustomApi", R.string.CustomApi));
+        otherItem.addSubItem(menu_custom_api, R.drawable.baseline_vpn_key_24, LocaleController.getString("CustomApi", R.string.CustomApi));
         otherItem.addSubItem(menu_custom_dc, R.drawable.baseline_sync_24, LocaleController.getString("CustomBackend", R.string.CustomBackend));
 
         actionBar.setAllowOverlayTitle(true);

luvletter2333/NekoX

TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
@@ -43,6 +43,7 @@ import android.os.Bundle;
 import android.telephony.PhoneNumberUtils;
 //import android.telephony.TelephonyManager;
 import android.text.Editable;
+import android.text.InputFilter;
 import android.text.InputType;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
@@ -167,9 +168,14 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.StrUtil;
+import kotlin.Unit;
 import tw.nekomimi.nekogram.NekoXConfig;
+import tw.nekomimi.nekogram.ui.BottomBuilder;
 import tw.nekomimi.nekogram.ui.EditTextAutoFill;
 import tw.nekomimi.nekogram.utils.AlertUtil;
 import tw.nekomimi.nekogram.utils.ProxyUtil;
@@ -337,7 +343,7 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
     private static final int menu_language = 3;
     private static final int menu_bot_login = 4;
     private static final int menu_other = 5;
-    //private int menu_custom_api = 6;
+    private int menu_custom_api = 6;
     private int menu_custom_dc = 7;
     private static final int menu_qr_login = 8;
 
@@ -686,7 +692,7 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
                 .setContentDescription(LocaleController.getString("ImportLogin", R.string.ImportLogin));
         menu.addSubItem(menu_custom_dc, R.drawable.baseline_sync_24, LocaleController.getString("CustomBackend", R.string.CustomBackend))
                 .setContentDescription(LocaleController.getString("CustomBackend", R.string.CustomBackend));
-//        otherItem.addSubItem(menu_custom_api, R.drawable.baseline_vpn_key_24, LocaleController.getString("CustomApi", R.string.CustomApi));
+        menu.addSubItem(menu_custom_api, R.drawable.baseline_vpn_key_24, LocaleController.getString("CustomApi", R.string.CustomApi));
 //        menu.addSubItem(menu_custom_dc, R.drawable.baseline_sync_24, LocaleController.getString("CustomBackend", R.string.CustomBackend));
 
         menu.setOnClickListener(v -> {
@@ -710,6 +716,8 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
                     else
                         phoneView.testBackendCheckBox.setVisibility(View.GONE);
                 }
+            } else if (id == menu_custom_api) {
+                setCustomApi();
             }
         });
         menu.setContentDescription(LocaleController.getString(R.string.items_other));
@@ -7884,6 +7892,107 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
         } else {
             onAuthSuccess((TLRPC.TL_auth_authorization) authorization);
         }
+    }
+
+
+    private void setCustomApi() {
+        AtomicInteger targetApi = new AtomicInteger(-1);
+        BottomBuilder builder = new BottomBuilder(getParentActivity());
+        EditText[] inputs = new EditText[2];
+        builder.addTitle(LocaleController.getString("CustomApi", R.string.CustomApi),
+                true,
+                LocaleController.getString("UseCustomApiNotice", R.string.UseCustomApiNotice));
+
+        builder.addRadioItem(LocaleController.getString("CustomApiNo", R.string.CustomApiNo), NekoXConfig.customApi == 0, (cell) -> {
+            targetApi.set(0);
+            builder.doRadioCheck(cell);
+            for (EditText input : inputs) input.setVisibility(View.GONE);
+            return Unit.INSTANCE;
+        });
+        builder.addRadioItem(LocaleController.getString("CustomApiOfficial", R.string.CustomApiOfficial), NekoXConfig.customApi == 1, (cell) -> {
+            targetApi.set(1);
+            builder.doRadioCheck(cell);
+            for (EditText input : inputs) input.setVisibility(View.GONE);
+            return Unit.INSTANCE;
+        });
+        builder.addRadioItem(LocaleController.getString("CustomApiTGX", R.string.CustomApiTGX), NekoXConfig.customApi == 2, (cell) -> {
+            targetApi.set(2);
+            builder.doRadioCheck(cell);
+            for (EditText input : inputs) input.setVisibility(View.GONE);
+            return Unit.INSTANCE;
+        });
+        builder.addRadioItem(LocaleController.getString("CustomApiInput", R.string.CustomApiInput), NekoXConfig.customApi > 2, (cell) -> {
+            targetApi.set(3);
+            builder.doRadioCheck(cell);
+            for (EditText input : inputs) input.setVisibility(View.VISIBLE);
+            return Unit.INSTANCE;
+        });
+        inputs[0] = builder.addEditText("App Id");
+        inputs[0].setInputType(InputType.TYPE_CLASS_NUMBER);
+        if (NekoXConfig.customAppId != 0) {
+            inputs[0].setText(NekoXConfig.customAppId + "");
+        }
+        inputs[0].addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (StrUtil.isBlank(s.toString())) {
+                    NekoXConfig.customAppId = 0;
+                } else if (!NumberUtil.isInteger(s.toString())) {
+                    inputs[0].setText("0");
+                } else {
+                    NekoXConfig.customAppId = NumberUtil.parseInt(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        inputs[1] = builder.addEditText("App Hash");
+        inputs[1].setFilters(new InputFilter[]{new InputFilter.LengthFilter(BuildVars.OFFICAL_APP_HASH.length())});
+        if (StrUtil.isNotBlank(NekoXConfig.customAppHash)) {
+            inputs[1].setText(NekoXConfig.customAppHash);
+        }
+        inputs[1].addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                NekoXConfig.customAppHash = s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        if (NekoXConfig.customApi <= 2) {
+            for (EditText input : inputs) input.setVisibility(View.GONE);
+        }
+        builder.addCancelButton();
+        builder.addButton(LocaleController.getString("Set", R.string.Set), (it) -> {
+            int target = targetApi.get();
+            if (target > 2) {
+                if (NekoXConfig.customAppId == 0) {
+                    inputs[0].requestFocus();
+                    AndroidUtilities.showKeyboard(inputs[0]);
+                    return Unit.INSTANCE;
+                } else if (StrUtil.isBlank(NekoXConfig.customAppHash)) {
+                    inputs[1].requestFocus();
+                    AndroidUtilities.showKeyboard(inputs[1]);
+                    return Unit.INSTANCE;
+                }
+            }
+            NekoXConfig.customApi = target;
+            NekoXConfig.saveCustomApi();
+            return Unit.INSTANCE;
+        });
+        builder.show();
     }
 
 }